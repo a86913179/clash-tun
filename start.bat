05029d5de03a2adedf5eb66c8043288b33033dd6
a86913179 <a86913179@gmail.com>
2020-12-31 18:11:09 +0800

    Add files via upload

--- start.bat
+++ start.bat
@@ -9,5 +9,5 @@ call clash.vbs
 
 for /f "delims=," %%a in ('Getmac /v /nh /fo csv') do (
   netsh interface ipv4 set dnsservers %%a static 198.18.0.2 validate=no
-  netsh interface ipv6 set dnsservers %%a static ::1 validate=no
+  netsh interface ipv6 set interface %%a routerdiscovery=disabled
 )
--- stop.bat
+++ stop.bat
@@ -9,5 +9,5 @@ taskkill /f /im clash.exe /t
 
 for /f "delims=," %%a in ('Getmac /v /nh /fo csv') do (
   netsh interface ipv4 set dnsservers %%a dhcp validate=no
-  netsh interface ipv6 set dnsservers %%a dhcp validate=no
+  netsh interface ipv6 set interface %%a routerdiscovery=enabled
 )
