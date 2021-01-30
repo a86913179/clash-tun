88419d2e368411c623a7e134f0fb435e8f19ea10
a86913179 <a86913179@gmail.com>
2021-01-30 16:29:47 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -5,7 +5,7 @@ setlocal
 
 set PATH=%PATH%;%SystemRoot%\system32;%SystemRoot%\system32\wbem;%SystemRoot%\system32\WindowsPowerShell/v1.0
 
-call clash.vbs
+start clash.vbs
 
 for /f "delims=," %%a in ('Getmac /v /nh /fo csv') do (
   netsh interface ipv4 set dnsservers %%a static 198.18.0.2 validate=no
