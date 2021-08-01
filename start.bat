fac354b1b89cf9bb2e0c252d26f3f8837a0e2ab2
a86913179 <a86913179@gmail.com>
2021-08-01 16:18:09 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -6,13 +6,13 @@ setlocal
 
 set PATH=%PATH%;%SystemRoot%\system32;%SystemRoot%\system32\wbem;%SystemRoot%\system32\WindowsPowerShell/v1.0
 
+start clash.vbs
+
 for /f "delims=," %%a in ('Getmac /v /nh /fo csv') do (
   netsh interface ipv4 set dnsservers %%a static 198.18.0.2 validate=no
   netsh interface ipv6 set interface %%a routerdiscovery=disabled
 )
 
 ipconfig /release6
-
-start clash.vbs
 
 ipconfig /flushdns
