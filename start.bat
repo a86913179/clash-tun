25c9eb01f94ba8df6da61b1adbe9fe82f0f423a2
a86913179 <a86913179@gmail.com>
2021-12-24 17:24:03 +0800

    Update

--- start.bat
+++ start.bat
@@ -6,16 +6,16 @@ setlocal
 
 set PATH=%PATH%;%SystemRoot%\system32;%SystemRoot%\system32\wbem;%SystemRoot%\system32\WindowsPowerShell/v1.0
 
+start clash.vbs
+
+timeout /t 5 /nobreak > nul
+
 for /f "delims=," %%a in ('Getmac /v /nh /fo csv') do (
   netsh interface ipv4 set dnsservers %%a static 198.18.0.2 validate=no
   netsh interface ipv6 set interface %%a routerdiscovery=disabled
 )
 
 ipconfig /release6
-
-start clash.vbs
-
-timeout /t 5 /nobreak > nul
 
 ipconfig /renew
 
--- stop.bat
+++ stop.bat
@@ -6,16 +6,16 @@ setlocal
 
 set PATH=%PATH%;%SystemRoot%\system32;%SystemRoot%\system32\wbem;%SystemRoot%\system32\WindowsPowerShell/v1.0
 
-taskkill /f /im clash.exe /t
-
-timeout /t 5 /nobreak > nul
-
 for /f "delims=," %%a in ('Getmac /v /nh /fo csv') do (
   netsh interface ipv4 set dnsservers %%a dhcp validate=no
   netsh interface ipv6 set interface %%a routerdiscovery=enabled
 )
 
 ipconfig /renew6
+
+taskkill /f /im clash.exe /t
+
+timeout /t 5 /nobreak > nul
 
 ipconfig /renew
 
